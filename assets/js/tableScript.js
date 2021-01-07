1571aba6d5956f5826a64080aab5809b9c451886
James <jek030@bucknell.edu>
2021-01-07 17:23:12 -0500

    sorting buttonsgit add index.html assets/js/tableScript.js git add index.html assets/js/tableScript.js git add index.html assets/js/tableScript.js git add index.html assets/js/tableScript.js !

--- assets/js/tableScript.js
+++ assets/js/tableScript.js
@@ -1,5 +1,6 @@
 //asynchronously load the file, change true to false for synchronous
 //https://www.artylope.com/blog/2016/12/28/load-a-local-json.html
+//https://stackoverflow.com/questions/881510/sorting-json-by-values
 function loadJSON(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -12,51 +13,75 @@ function loadJSON(file, callback) {
     rawFile.send(null);
 }
 
+function sortJSON(arr, key, way) {
+    return arr.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        if (way === 'descending') { return ((x < y) ? -1 : ((x > y) ? 1 : 0)); }
+        if (way === 'ascending') { return ((x > y) ? -1 : ((x < y) ? 1 : 0)); }
+    });
+}
+// ------ helper functions ------------------
+var sortMethod;
+var sortArrangement;
+
+function setStock(){
+    sortMethod = "stock";
+}
+
+function set10DayReturn(){
+    sortMethod = "10 day return";
+}
+
+function set30DayReturn(){
+    sortMethod = "30 day return";
+}
+
+function setAscending(){
+    sortArrangement = "ascending";
+}
+function setDescending(){
+    sortArrangement = "descending";
+}
+
 function createJamesCurStocksTable(){
     
     loadJSON('jamesCurStocks.json', function(data) {
         var stockList = JSON.parse(data);
         stockList = stockList["stocks"];
-           $('#one').children().remove(".table");
-            var myTable= "<table><tr><td style='width: 100px; color: red;'><strong>Stock</strong></td>";
-            myTable+= "<td style='width: 100px; color: red; text-align: right;'><strong>10-Day % Return</strong></td>";
-            myTable+="<td style='width: 100px; color: red; text-align: right;'><strong>30-Day % Return</strong></td></tr>";
+        stockList = sortJSON(stockList, sortMethod, sortArrangement);
+        $('#one').children().remove(".table");
+        var myTable= "<table><tr><td style='width: 100px; color: red;'><strong>Stock</strong></td>";
+        myTable+= "<td style='width: 100px; color: red; text-align: right;'><strong>10-Day % Return</strong></td>";
+        myTable+="<td style='width: 100px; color: red; text-align: right;'><strong>30-Day % Return</strong></td></tr>";
 
 
-              for (var i=0; i<stockList.length; i++) {
-                myTable+="<tr><td style='width: 100px;'>" + stockList[i]["stock"] + ":</td>";
-                
-                myTable+="<td style='width: 100px; text-align: right;'>" + stockList[i]["10 day return"].toFixed(2) + "%</td>";
-                myTable+="<td style='width: 100px; text-align: right;'>" + stockList[i]["30 day return"].toFixed(2) + "%</td></tr>"; 
-                //if table disappears something above is wrong
-            }  
+            for (var i=0; i<stockList.length; i++) {
+            myTable+="<tr><td style='width: 100px;'>" + stockList[i]["stock"] + ":</td>";
             
-            myTable+="</table>";
-
-            var tempDiv = document.createElement('div');
-            tempDiv.className = "table";
-            tempDiv.innerHTML = myTable;
+            myTable+="<td style='width: 100px; text-align: right;'>" + stockList[i]["10 day return"].toFixed(2) + "%</td>";
+            myTable+="<td style='width: 100px; text-align: right;'>" + stockList[i]["30 day return"].toFixed(2) + "%</td></tr>"; 
+            //if table disappears something above is wrong
+        }  
         
-            var prependedData = $('#one').children().last()[0];
-            document.getElementById("one").appendChild(tempDiv);
+        myTable+="</table>";
+
+        var tempDiv = document.createElement('div');
+        tempDiv.className = "table";
+        tempDiv.innerHTML = myTable;
+    
+        var prependedData = $('#one').children().last()[0];
+        document.getElementById("one").appendChild(tempDiv);
     });
     
 }
 
-function sortJSON(arr, key, way) {
-    return arr.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        if (way === '123') { return ((x < y) ? -1 : ((x > y) ? 1 : 0)); }
-        if (way === '321') { return ((x > y) ? -1 : ((x < y) ? 1 : 0)); }
-    });
-}
 
 function createDOWStocksTable(){
     
     loadJSON('DOWstocks.json', function(data) {
         var stockList = JSON.parse(data);
         stockList = stockList["stocks"];
-        stockList = sortJSON(stockList, '10 day return', '321');
+        stockList = sortJSON(stockList, sortMethod, sortArrangement);
         //stockList = sortJSON(stockList, '30 day return', '321');
         //stockList = sortJSON(stockList, 'stock', '123');
         
@@ -93,27 +118,28 @@ function createSandPStocksTable(){
         var stockList = JSON.parse(data);
         console.log(stockList)
         stockList = stockList["stocks"];
-           $('#one').children().remove(".table");
-            var myTable= "<table><tr><td style='width: 100px; color: red;'><strong>Stock</strong></td>";
-            myTable+= "<td style='width: 100px; color: red; text-align: right;'><strong>10-Day % Return</strong></td>";
-            myTable+="<td style='width: 100px; color: red; text-align: right;'><strong>30-Day % Return</strong></td></tr>";
+        stockList = sortJSON(stockList, sortMethod, sortArrangement);
+        $('#one').children().remove(".table");
+        var myTable= "<table><tr><td style='width: 100px; color: red;'><strong>Stock</strong></td>";
+        myTable+= "<td style='width: 100px; color: red; text-align: right;'><strong>10-Day % Return</strong></td>";
+        myTable+="<td style='width: 100px; color: red; text-align: right;'><strong>30-Day % Return</strong></td></tr>";
 
 
-              for (var i=0; i<stockList.length; i++) {
-                myTable+="<tr><td style='width: 100px;'>" + stockList[i]["stock"] + ":</td>";
-                myTable+="<td style='width: 100px; text-align: right;'>" + Number(stockList[i]["10 day return"]).toFixed(2) + "%</td>";
-                myTable+="<td style='width: 100px; text-align: right;'>" + Number(stockList[i]["30 day return"]).toFixed(2) + "%</td></tr>"; 
-                //if table disappears something above is wrong
-            }  
-            
-            myTable+="</table>";
-
-            var tempDiv = document.createElement('div');
-            tempDiv.className = "table";
-            tempDiv.innerHTML = myTable;
+            for (var i=0; i<stockList.length; i++) {
+            myTable+="<tr><td style='width: 100px;'>" + stockList[i]["stock"] + ":</td>";
+            myTable+="<td style='width: 100px; text-align: right;'>" + Number(stockList[i]["10 day return"]).toFixed(2) + "%</td>";
+            myTable+="<td style='width: 100px; text-align: right;'>" + Number(stockList[i]["30 day return"]).toFixed(2) + "%</td></tr>"; 
+            //if table disappears something above is wrong
+        }  
         
-            var prependedData = $('#one').children().last()[0];
-            document.getElementById("one").appendChild(tempDiv);
+        myTable+="</table>";
+
+        var tempDiv = document.createElement('div');
+        tempDiv.className = "table";
+        tempDiv.innerHTML = myTable;
+    
+        var prependedData = $('#one').children().last()[0];
+        document.getElementById("one").appendChild(tempDiv);
     });
     
 }
